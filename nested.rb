2372bf5642c7f1d9ef99fb99c55e761193869f3f
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 16:40:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages].strip
+  programmer_hash[:dennis_ritchie][:languages].gets.strip
 end
 
 def adding_matz
